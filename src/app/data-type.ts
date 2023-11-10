465e2b918ff31ac42919070c0a117c6175f9cccf
tharanga270 <developerwordpress270@gmail.com>
2023-11-10 21:32:06 +0530

    11

--- src/app/data-type.ts
+++ src/app/data-type.ts
@@ -7,3 +7,12 @@ export interface login {
   email: String;
   password: String;
 }
+
+export interface product{
+  name:string,
+  price:number,
+  category:string,
+  color:string,
+  description:string,
+  image:string,
+}
--- src/app/seller-add-product/seller-add-product.component.ts
+++ src/app/seller-add-product/seller-add-product.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { product } from '../data-type';
 
 @Component({
   selector: 'app-seller-add-product',
@@ -6,11 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./seller-add-product.component.scss'],
 })
 export class SellerAddProductComponent {
-  constructor() {}
+  addProductMessage: string | undefined;
+  constructor(private product: ProductService) {}
 
   ngOnInit(): void {}
 
-  submit(data: object) {
-    console.warn(data);
+  submit(data: product) {
+    this.product.addProduct(data).subscribe((result) => {
+      console.warn(result);
+      if (result) {
+        this.addProductMessage = 'Product is added successfully';
+      }
+      setTimeout(() => {
+        this.addProductMessage = undefined;
+      }, 3000);
+    });
   }
 }
