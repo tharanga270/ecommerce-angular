1328f58aff08ca1d0e64fb055a1be21caca7a7a9
tharanga270 <developerwordpress270@gmail.com>
2023-11-10 18:30:18 +0530

    8.Done with seller menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  menuType: String = 'default';
+  constructor(private route: Router) {}
 
+  ngOnInit(): void {
+    this.route.events.subscribe((val: any) => {
+      if(val.url){
+        if(localStorage.getItem('seller') && val.url.includes('seller')){
+          console.warn('this is seller area');
+          this.menuType = 'seller';
+        }else{
+          console.warn("outside seller");
+          this.menuType = 'default';
+        }
+      }
+    });
+  }
 }
