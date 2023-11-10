34acaca51e791e7acd694046dc8bf22fd066f8eb
tharanga270 <developerwordpress270@gmail.com>
2023-11-10 19:42:34 +0530

    10.Done with add product UI

--- src/app/seller-add-product/seller-add-product.component.ts
+++ src/app/seller-add-product/seller-add-product.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-seller-add-product',
   templateUrl: './seller-add-product.component.html',
-  styleUrls: ['./seller-add-product.component.scss']
+  styleUrls: ['./seller-add-product.component.scss'],
 })
 export class SellerAddProductComponent {
+  constructor() {}
 
+  ngOnInit(): void {}
+
+  submit(data: object) {
+    console.warn(data);
+  }
 }
