c5e4299af791c7631ea529491fc87a01eb4693f3
tharanga270 <developerwordpress270@gmail.com>
2023-11-11 19:33:43 +0530

    13.Done with delete api

--- src/app/data-type.ts
+++ src/app/data-type.ts
@@ -8,11 +8,12 @@ export interface login {
   password: String;
 }
 
-export interface product{
-  name:string,
-  price:number,
-  category:string,
-  color:string,
-  description:string,
-  image:string,
+export interface product {
+  name: string;
+  price: number;
+  category: string;
+  color: string;
+  description: string;
+  image: string;
+  id: number;
 }
--- src/app/seller-home/seller-home.component.ts
+++ src/app/seller-home/seller-home.component.ts
@@ -9,12 +9,31 @@ import { product } from '../data-type';
 })
 export class SellerHomeComponent implements OnInit {
   productList: undefined | product[];
+  productMessage: undefined | string;
   constructor(private product: ProductService) {}
 
   ngOnInit(): void {
+    this.list();
+  }
+
+  deleteProduct(id: number) {
+    this.product.deleteProduct(id).subscribe((result) => {
+      if (result) {
+        this.productMessage = 'Product is deleted';
+
+        this.list();
+      }
+    });
+    setTimeout(() => {
+      this.productMessage = undefined;
+    }, 3000);
+  }
+
+  list() {
     this.product.productList().subscribe((result) => {
-      console.warn(result);
-      this.productList=result;
+      if (result) {
+        this.productList = result;
+      }
     });
   }
 }
