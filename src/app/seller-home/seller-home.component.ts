5feb1acc1da5939cf486f8defef22d521e8cfd9e
tharanga270 <developerwordpress270@gmail.com>
2023-11-11 20:55:56 +0530

    14.Done with icons

--- src/app/seller-home/seller-home.component.ts
+++ src/app/seller-home/seller-home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { product } from '../data-type';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-seller-home',
@@ -10,6 +11,7 @@ import { product } from '../data-type';
 export class SellerHomeComponent implements OnInit {
   productList: undefined | product[];
   productMessage: undefined | string;
+  icon = faTrash;
   constructor(private product: ProductService) {}
 
   ngOnInit(): void {
