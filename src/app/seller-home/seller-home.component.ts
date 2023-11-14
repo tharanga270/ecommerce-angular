3eb668c239dec8d8d9e51ca18f1e582bc863bb2b
tharanga270 <developerwordpress270@gmail.com>
2023-11-14 17:18:40 +0530

    15.Done with update product UI

--- src/app/seller-home/seller-home.component.ts
+++ src/app/seller-home/seller-home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { product } from '../data-type';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faTrash, faEdit } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-seller-home',
@@ -12,6 +12,8 @@ export class SellerHomeComponent implements OnInit {
   productList: undefined | product[];
   productMessage: undefined | string;
   icon = faTrash;
+  iconEdit=faEdit;
+
   constructor(private product: ProductService) {}
 
   ngOnInit(): void {
---
+++ src/app/seller-update-product/seller-update-product.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seller-update-product',
+  templateUrl: './seller-update-product.component.html',
+  styleUrls: ['./seller-update-product.component.scss'],
+})
+export class SellerUpdateProductComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+  submit(data: any) {}
+}
