915d2ca0e5113dab7463a0eb0088d9cfd405643e
tharanga270 <developerwordpress270@gmail.com>
2023-11-14 20:30:40 +0530

    18.Done with bootstrap

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  constructor() {}
 
+  ngOnInit(): void {
+  }
 }
